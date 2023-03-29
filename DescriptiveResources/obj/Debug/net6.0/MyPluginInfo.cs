abc3f9a43967aee2d2c6c8a1daf7523e62b47e45
eyesoffaith <71423920+eyesoffaith@users.noreply.github.com>
2023-03-29 15:37:52 -0400

    Delete DescriptiveResources/obj directory

--- DescriptiveResources/obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace OneHitResource
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "OneHitResource";
-        public const string PLUGIN_NAME = "OneHitResource";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
