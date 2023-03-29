ae420dc9a57a3591bb626b80c8a35e57a79972ea
eyesoffaith <71423920+eyesoffaith@users.noreply.github.com>
2023-03-29 15:36:10 -0400

    Delete StackingResourceStorage directory

--- StackingResourceStorage/obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace StackingResourceStorage
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "StackingResourceStorage";
-        public const string PLUGIN_NAME = "StackingResourceStorage";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
