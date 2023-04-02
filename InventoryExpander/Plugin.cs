8c260effec10e710ed349f5b7e620b861de7486b
eyesoffaith <miles.daniell009@gmail.com>
2023-04-02 17:37:04 -0400

    more attempts at changing inventory capacity

--- InventoryExpander/Plugin.cs
+++ InventoryExpander/Plugin.cs
@@ -6,9 +6,14 @@ using HarmonyLib.Tools;
 using System;
 using System.Reflection;
 using System.Collections.Generic;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
 
 namespace InventoryExpander;
 
+// TODO: Attempt manually patching ItemStorageData constructor
+//     var ctor = (MethodBase)((typeof(ItemStorageData)).GetMember(".ctor", AccessTools.all))[0];
+//     harmony.Patch(ctor, postfix: new HarmonyMethod(typeof(Patch_ItemStorageData), "Postfix"));
+        
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BasePlugin
 {
@@ -21,35 +26,67 @@ public class Plugin : BasePlugin
             
         // Plugin startup logic
         Plugin.Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-
-        Plugin.Logger.LogMessage($"ItemStorageData Methods");
+        Plugin.Logger.LogInfo($"ItemStorageData Methods");
         foreach (var method in typeof(ItemStorageData).GetMethods()) {
-            Plugin.Logger.LogMessage(method);
+            Plugin.Logger.LogInfo(method);
         }
-        Plugin.Logger.LogMessage($"ItemStorageData Properties");
+        Plugin.Logger.LogInfo($"ItemStorageData Properties");
         foreach (var property in typeof(ItemStorageData).GetProperties()) {
-            Plugin.Logger.LogMessage(property);
+            Plugin.Logger.LogInfo(property);
         }
-
         this.Awake();
     }
 
     public void Awake()
     {
         Harmony harmony = new Harmony("InventoryExpander");
+
         harmony.PatchAll();
     }
 }
 
-[HarmonyPatch(typeof(ItemStorageData), "SetMaterialMaxHaveNumAllDigimonPartner")]
-class Patch_SetMaterialMaxHaveNumAllDigimonPartner
+[HarmonyPatch(typeof(ItemStorageData), "InitializeItemList")]
+[HarmonyPatch(new Type[] { typeof(ItemStorageData.StorageType) })]
+class Patch_InitializeItemList
 {
-    static bool Prefix(ItemStorageData __instance)
+    static void Postfix(ItemStorageData __instance)
     {
-        Traverse.Create(__instance).Property("m_maxMaterialHaveNum").SetValue(600);
-        return false;
+        Plugin.Logger.LogMessage("[ItemStorageData::Patch_InitializeItemList::Prefix]");
+        int[] newTypeMax = {60, 500, 600, 20};
+        Traverse.Create(__instance).Property("m_itemTypeMaxTbl").SetValue(new Il2CppStructArray<int>(newTypeMax));
+
+        dynamic m_itemDataListTbl = Traverse.Create(__instance).Property("m_itemDataListTbl").GetValue();
+        for (int i = 0; i < m_itemDataListTbl.Count; i++) {
+            int oldCap = m_itemDataListTbl[i].Capacity;
+            m_itemDataListTbl[i].Capacity = newTypeMax[i];
+
+            if (newTypeMax[i] > oldCap) {
+                for (i = 0; i < newTypeMax[i] - oldCap; i++) {
+                    m_itemDataListTbl[i].Add(new ItemData());
+                }
+            }
+
+            Plugin.Logger.LogMessage($"m_itemDataListTbl[i].Capacity = {m_itemDataListTbl[i].Capacity}");
+        }
     }
 }
+
+// [HarmonyPatch(typeof(ItemStorageData), "CreateInitItemList")]
+// [HarmonyPatch(new Type[] { typeof(List<ItemData>), typeof(int) }, new ArgumentType[] { ArgumentType.Ref, ArgumentType.Normal })]
+// class Patch_CreateInitItemList
+// {
+//     static void Postfix(ref List<ItemData> list, int max_capacity, ItemStorageData __instance)
+//     {
+//         Plugin.Logger.LogMessage("[ItemStorageData::Patch_CreateInitItemList::Postfix]");
+//         Plugin.Logger.LogMessage($"max_capacity = {max_capacity}");
+//         List<ItemData>[] m_itemDataListTbl = (List<ItemData>[])Traverse.Create(__instance).Property("m_itemDataListTbl").GetValue();
+
+//         Plugin.Logger.LogMessage($"list.Capacity = {list.Capacity}");
+//         foreach(var _list in m_itemDataListTbl) {
+//             Plugin.Logger.LogMessage($"list.Capacity = {_list.Capacity}");
+//         }
+//     }
+// }
 
 [HarmonyPatch(typeof(ItemStorageData))]
 [HarmonyPatch("GetTypeMaxNum")]
