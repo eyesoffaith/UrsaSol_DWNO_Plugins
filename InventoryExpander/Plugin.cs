85bf3db754819f6fe8932f7f6ce909475f477aad
eyesoffaith <miles.daniell009@gmail.com>
2023-04-05 21:14:31 -0400

    first successful inventory modification past the 200 limit

--- InventoryExpander/Plugin.cs
+++ InventoryExpander/Plugin.cs
@@ -11,41 +11,28 @@ using Il2CppInterop.Runtime.InteropTypes.Arrays;
 
 namespace InventoryExpander;
 
-// TODO: Attempt to call ItemStorageData::SaveItemListBase from Patch_PickMaterial::Postfix
+// ACCOMPLISHMENTS:
+//  - Found some success by adding ItemData directly to ItemStorageData.m_itemDataListTbl. Would be nice to not have to bypass the original implementation though
+
+// TODO: All item collection, card collection, and item storage is handled by a delegate stored in uItemPickPanel.m_itemPickCallback
+//  - Create PickMaterial Postfix that overwrites uItemPickPanel.m_itemPickCallback with an equivalent implementation we can tweak OR
+//  - Learn how to write a Transpiler that can successfully cut out the StorageData.m_ItemStorageData.SaveItemList in the current delegate and implement it ourselves
         
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BasePlugin
 {
     public static ManualLogSource Logger;
     public static StorageData StorageData;
+    public static Type ListType;
     
     public override void Load()
     {
         Plugin.Logger = base.Log;
         Plugin.StorageData = new StorageData();
 
+
         HarmonyFileLog.Enabled = true;
         Plugin.Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-
-        // // Plugin startup logic
-        // Plugin.Logger.LogInfo($"ItemStorageData Methods");
-        // foreach (var method in typeof(ItemStorageData).GetMethods()) {
-        //     Plugin.Logger.LogInfo(method);
-        // }
-        // Plugin.Logger.LogInfo($"ItemStorageData Properties");
-        // foreach (var property in typeof(ItemStorageData).GetProperties()) {
-        //     Plugin.Logger.LogInfo(property);
-        // }
-
-        // Plugin.Logger.LogInfo($"StorageData Methods");
-        // foreach (var method in typeof(StorageData).GetMethods()) {
-        //     Plugin.Logger.LogInfo(method);
-        // }
-        // Plugin.Logger.LogInfo($"StorageData Properties");
-        // foreach (var property in typeof(StorageData).GetProperties()) {
-        //     Plugin.Logger.LogInfo(property);
-        // }
-
         this.Awake();
     }
 
@@ -76,82 +63,17 @@ class Patch_InitializeItemList
                     m_itemDataListTbl[i].Add(new ItemData());
                 }
             }
-
-            Plugin.Logger.LogMessage($"m_itemDataListTbl[i].Capacity = {m_itemDataListTbl[i].Capacity}");
         }
     }
 }
-[HarmonyPatch(typeof(ItemStorageData), "AddItemPlayer")]
-public static class Patch_AddItemPlayer
-{
-    public static bool Prefix() 
-    {
-        Plugin.Logger.LogMessage("[ItemStorageData::Patch_AddItemPlayer::Prefix]");
 
-        return true;
-    }
-
-    public static void Postfix() 
-    {
-        Plugin.Logger.LogMessage("[ItemStorageData::Patch_AddItemPlayer::Postfix]");
-    }
-}
-
-[HarmonyPatch(typeof(uItemPickPanel), "PickMaterial")]
-public static class Patch_PickMaterial
-{
-    public static bool Prefix() 
-    {
-        Plugin.Logger.LogMessage("[uItemPickPanel::Patch_PickMaterial::Prefix]");
-
-        return true;
-    }
-
-    public static void Postfix() 
-    {
-        Plugin.Logger.LogMessage("[uItemPickPanel::Patch_PickMaterial::Postfix]");
-
-        dynamic m_itemPickPoint = ItemPickPointManager.Ref.PickingPoint;
-        dynamic m_itemIds = m_itemPickPoint.PickItem(10, 1.2f);
-        dynamic m_ItemStorageData = Traverse.Create(Plugin.StorageData).Property("m_ItemStorageData").GetValue();
-        dynamic m_itemList = m_ItemStorageData.GetItemListKindBit(ItemStorageData.StorageType.MATERIAL, ParameterItemData.KindIndexBit.KindMaterialBit);
-        
-        Plugin.Logger.LogMessage($"m_itemList.Count = {m_itemList.Count}");
-
-        for (int k = 0; k < m_itemIds.Length; k++)
-        {
-            ItemData item = new ItemData(m_itemIds[k], 1);
-            m_itemList.Add(item);
-        }
-        Plugin.Logger.LogMessage($"m_itemList.Count = {m_itemList.Count}");
-
-        Plugin.Logger.LogMessage($"{ItemStorageData.StorageType.MATERIAL}");
-        Plugin.Logger.LogMessage($"m_itemList.GetType() = {m_itemList.GetType()}");
-
-        m_ItemStorageData.SaveItemList(ItemStorageData.StorageType.MATERIAL, ref m_itemList);
-
-        Plugin.Logger.LogMessage($"m_itemList.Count = {m_itemList.Count}");
-    }
-}
-
-[HarmonyPatch(typeof(ItemStorageData))]
-[HarmonyPatch("GetTypeMaxNum")]
+[HarmonyPatch(typeof(ItemStorageData), "GetTypeMaxNum")]
 [HarmonyPatch(new Type[] { typeof(ItemStorageData.StorageType) }, new ArgumentType[] { ArgumentType.Ref })]
 public static class Patch_GetTypeMaxNumRef
 {
     public static void Postfix(ItemStorageData.StorageType type, ref int __result, ItemStorageData __instance) 
     {
         Plugin.Logger.LogMessage("[ItemStorageData::Patch_GetTypeMaxNumRef::Postfix]");
-        Plugin.Logger.LogMessage($"type = {type}");
-
-        if (type == ItemStorageData.StorageType.MATERIAL) {
-            System.Diagnostics.StackTrace t = new System.Diagnostics.StackTrace();
-            for (int i = 0; i < t.FrameCount; i++) {
-                StackFrame sf = t.GetFrame(i);
-                Plugin.Logger.LogMessage($"{sf.GetMethod()}@{sf.GetFileLineNumber()}");
-            }
-        }
-
         switch(type)
         {
             case ItemStorageData.StorageType.KEY_ITEM:
