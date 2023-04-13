201887614041a7ae60182a7c4b9b493b7ca89237
eyesoffaith <miles.daniell009@gmail.com>
2023-04-12 20:59:46 -0400

    OneHitResource v1.1.4 - Added config parameters to toggle the mod, multiplier resources, and increase rarity

--- OneHitResource/Plugin.cs
+++ OneHitResource/Plugin.cs
@@ -19,14 +19,21 @@ public class Plugin : BasePlugin
 {
     public static ManualLogSource Logger;
     public static MethodInfo unpatched_PickItem;
+    
+    public static ConfigEntry<float> resourceRarity;
+    public static ConfigEntry<float> resourceMultiplier;
     public static ConfigEntry<int> cardProbability;
     public static ConfigEntry<bool> breakNodeFullInventory;
+    public static ConfigEntry<bool> oneHitNode;
 
     public override void Load()
     {
         Plugin.Logger = base.Log;
         HarmonyFileLog.Enabled = true;
-
+        
+        Plugin.oneHitNode = base.Config.Bind<bool>("One Hit Resource Node", "enable", true, "Whether the mod should gather all materials from a resource node at once.");
+        Plugin.resourceMultiplier = base.Config.Bind<float>("Resource Multiplier", "multiplier", 1, "Multiplies the number of materials pulled from resource nodes.");
+        Plugin.resourceRarity = base.Config.Bind<float>("Resource Rarity Multiplier", "multiplier", 1, "Multiplies the chance of pulling rare materials from resource nodes.");
         Plugin.cardProbability = base.Config.Bind<int>("Card Drop Chance", "probability", 10, "Percent chance of a card dropping from resource node per material pull.");
         Plugin.breakNodeFullInventory = base.Config.Bind<bool>("Break Material Node Toggle", "breakNodeFullInventory", false, "Run card chance and break material node even when inventory is full.");
 
@@ -48,19 +55,21 @@ public class Plugin : BasePlugin
 public static class Patch_PickItem
 {
     public static bool Prefix() {
-        return false;
+        return !Plugin.oneHitNode.Value;
     }
 
     public static void Postfix(int requestPickCount, float rarityRevision, ref dynamic __result, ItemPickPointTimeRebirth __instance) {
-        List<UInt32> materials = new List<UInt32>();
-        int remainderPickCount = __instance.remainderPickCount;
-        for (int i = 0; i < remainderPickCount; i++) {
-            dynamic part = Plugin.unpatched_PickItem.Invoke(__instance, new object[] { __instance, requestPickCount, rarityRevision });
-            foreach (var item in part) {
-                materials.Add(item);
+        if (Plugin.oneHitNode.Value) {
+            List<UInt32> materials = new List<UInt32>();
+            int remainderPickCount = __instance.remainderPickCount;
+            for (int i = 0; i < remainderPickCount; i++) {
+                dynamic part = Plugin.unpatched_PickItem.Invoke(__instance, new object[] { __instance, (int)(Plugin.resourceMultiplier.Value * requestPickCount), (int)(Plugin.resourceRarity.Value * rarityRevision) });
+                foreach (var item in part) {
+                    materials.Add(item);
+                }
             }
+            __result = new Il2CppStructArray<UInt32>(materials.ToArray());
         }
-        __result = new Il2CppStructArray<UInt32>(materials.ToArray());
     }
 }
 
