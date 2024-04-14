1267a94e379789ea4b1d26f77a8a7ba576fcbc1c
Daniel Miles <miles.daniell009@gmail.com>
2024-04-14 15:07:13 -0400

    more notes on previous

--- ConversionOverhaul/Plugin.cs
+++ ConversionOverhaul/Plugin.cs
@@ -14,32 +14,15 @@ using UnityEngine.UI;
 
 namespace ConversionOverhaul;
 
-// TODO: Check ParameterCommonSelectWindowMode. There is a WindowType parameter that looks like a clue
-// * MaterialChange#
-// * LaboratoryItemChange##
-
-
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BasePlugin
 {
     public static ManualLogSource Logger;
     
-    // public static ConfigEntry<float> resourceRarity;
-    // public static ConfigEntry<float> resourceMultiplier;
-    // public static ConfigEntry<int> cardProbability;
-    // public static ConfigEntry<bool> breakNodeFullInventory;
-    // public static ConfigEntry<bool> oneHitNode;
-
     public override void Load()
     {
         Plugin.Logger = base.Log;
         HarmonyFileLog.Enabled = true;
-        
-        // Plugin.oneHitNode = base.Config.Bind<bool>("One Hit Resource Node", "enable", true, "Whether the mod should gather all materials from a resource node at once.");
-        // Plugin.resourceMultiplier = base.Config.Bind<float>("Resource Multiplier", "multiplier", 1, "Multiplies the number of materials pulled from resource nodes.");
-        // Plugin.resourceRarity = base.Config.Bind<float>("Resource Rarity Multiplier", "multiplier", 1, "Multiplies the chance of pulling rare materials from resource nodes.");
-        // Plugin.cardProbability = base.Config.Bind<int>("Card Drop Chance", "probability", 10, "Percent chance of a card dropping from resource node per material pull.");
-        // Plugin.breakNodeFullInventory = base.Config.Bind<bool>("Break Material Node Toggle", "breakNodeFullInventory", false, "Run card chance and break material node even when inventory is full.");
 
         // Plugin startup logic
         Plugin.Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
@@ -73,6 +56,7 @@ public static class Patch_MainGameManager_enableCommonSelectWindowUI
     }
 }
 
+// TODO: Investigate this method further. This was the one that triggered during investigation!
 [HarmonyPatch(typeof(ParameterCommonSelectWindowMode), "GetParam")]
 [HarmonyPatch(new Type[] { typeof(ParameterCommonSelectWindowMode.WindowType) })]
 public static class Patch_ParameterCommonSelectWindowMode_GetParam
