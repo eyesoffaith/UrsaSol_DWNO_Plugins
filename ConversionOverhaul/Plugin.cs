c1549aca61c7d0d7e805e0e86a96422d0f6f9359
Daniel Miles <miles.daniell009@gmail.com>
2024-04-15 15:30:44 -0400

    more notes

--- ConversionOverhaul/Plugin.cs
+++ ConversionOverhaul/Plugin.cs
@@ -50,29 +50,38 @@ LaboratoryItemChange02 = Haguromon (metal/wood hunt)
 Transmission = Birdramon
 */
 
-// TODO: Investigate this method further. This was the one that triggered during investigation!
-//  * are there any differences between patching static and non-static methods
+// TODO: Check out CScenarioScript class and it's CallCmdBlockCommonSelectWindow() method
 [HarmonyPatch(typeof(ParameterCommonSelectWindowMode), "GetParam")]
 [HarmonyPatch(new Type[] { typeof(ParameterCommonSelectWindowMode.WindowType) })]
 public static class Patch_ParameterCommonSelectWindowMode_GetParam
 {
-    public static void Postfix(ParameterCommonSelectWindowMode.WindowType window_type, ref dynamic __result, ParameterCommonSelectWindowMode __instance) {
+    public static void Postfix(ParameterCommonSelectWindowMode.WindowType window_type, ref dynamic __result) {
         Plugin.Logger.LogInfo($"ParameterCommonSelectWindowMode::GetParam");
         Plugin.Logger.LogInfo($"window_type {window_type}");
         Plugin.Logger.LogInfo($"__result {__result}");
-        Plugin.Logger.LogInfo($"__instance {__instance}");
+        Plugin.Logger.LogInfo($"m_type {__result.m_type}");
+        Plugin.Logger.LogInfo($"m_blockIndex {__result.m_blockIndex}");
+        Plugin.Logger.LogInfo($"m_bit {__result.m_bit}");
+        Plugin.Logger.LogInfo($"m_dailyQusetPoint {__result.m_dailyQusetPoint}");
+        Plugin.Logger.LogInfo($"m_coin {__result.m_coin}");
     }
 }
 
-[HarmonyPatch(typeof(ParameterCommonSelectWindow), "GetParam")]
-[HarmonyPatch(new Type[] { typeof(ParameterCommonSelectWindowMode.WindowType), typeof(int) })]
-public static class Patch_ParameterCommonSelectWindow_GetParam
+[HarmonyPatch(typeof(ParameterCommonSelectWindowMode), "GetCommonSelectWindowCsvb")]
+public static class Patch_ParameterCommonSelectWindowMode_GetCommonSelectWindowCsvb
 {
-    public static void Postfix(ParameterCommonSelectWindowMode.WindowType window_type, int record_index, ref dynamic __result, ParameterCommonSelectWindowMode __instance) {
+    public static void Postfix(ref dynamic __result, ParameterCommonSelectWindow __instance) {
         Plugin.Logger.LogInfo($"ParameterCommonSelectWindowMode::GetParam");
-        Plugin.Logger.LogInfo($"window_type {window_type}");
-        Plugin.Logger.LogInfo($"record_index {record_index}");
         Plugin.Logger.LogInfo($"__result {__result}");
         Plugin.Logger.LogInfo($"__instance {__instance}");
+
+        Plugin.Logger.LogInfo($"METHODS");
+        foreach (var info in __result.GetType().GetMethods()) {
+            Plugin.Logger.LogInfo(info);
+        }
+        Plugin.Logger.LogInfo($"PROPERTIES");
+        foreach (var info in __result.GetType().GetProperties()) {
+            Plugin.Logger.LogInfo(info);
+        }
     }
 }
